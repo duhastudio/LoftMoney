bc151d3dfc60b4bab3519b76aa3e27f0f49b69c9
DUHASTUDIO <duhastudio@gmail.com>
2018-08-28 03:22:44 +0300

    Add Activity

---
+++ app/src/main/java/com/duhastudio/loftmoney/AddActivity.java
@@ -0,0 +1,54 @@
+package com.duhastudio.loftmoney;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class AddActivity extends AppCompatActivity {
+
+
+    private EditText nameInput;
+    private EditText priceInput;
+    private Button addBtn;
+
+    @SuppressLint("WrongViewCast")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+
+        nameInput = findViewById(R.id.name);
+        priceInput = findViewById(R.id.price);
+        addBtn = findViewById(R.id.add_btn);
+
+        nameInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) { }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+             addBtn.setEnabled(TextUtils.isEmpty(editable));
+            }
+        });
+        priceInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) { }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                addBtn.setEnabled(!TextUtils.isEmpty(s));
+            }
+        });
+    }
+}
